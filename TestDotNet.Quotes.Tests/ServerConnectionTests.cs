072b62afb2306716abed64b04a258a1320f55d22
Leonardo Cardoso <kibiluzbad@hotmail.com>
2011-07-05 20:59:28 -0300

    First commit.

---
+++ TestDotNet.Quotes.Tests/ServerConnectionTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TestDotNet.Quotes.Tests
+{
+    [TestFixture]
+    public class ServerConnectionTests
+    {
+        [Test]
+        public void Can_Connect_To_Server()
+        {
+            // Don't forget to run packages\ClientAPI\Server.exe
+            var connection = new ClientAPI.ClientConnection();
+            var serverConnected = connection.Connect("127.0.0.1", 34567);
+
+            Assert.That(serverConnected, Is.True);
+        }
+
+        [Test]
+        public void Can_Disconnect_Form_Server()
+        {
+            // Don't forget to run packages\ClientAPI\Server.exe
+            var connection = new ClientAPI.ClientConnection();
+            connection.Connect("127.0.0.1", 34567);
+            var serverDisconnected = connection.Disconnect();
+
+            Assert.That(serverDisconnected, Is.True);
+        }
+
+        [Test]
+        public void Can_Login_To_Server()
+        {
+            // Don't forget to run packages\ClientAPI\Server.exe
+            var connection = new ClientAPI.ClientConnection();
+            connection.Connect("127.0.0.1", 34567);
+
+            var loggedIn = connection.Login("trademaker01", "t01m975kt");
+
+            connection.Disconnect();
+
+            Assert.That(loggedIn, Is.True);
+        }
+
+        [Test]
+        public void Can_Logout_From_Server()
+        {
+            // Don't forget to run packages\ClientAPI\Server.exe
+            var connection = new ClientAPI.ClientConnection();
+            connection.Connect("127.0.0.1", 34567);
+
+            connection.Login("trademaker01", "t01m975kt");
+            var loggedOut = connection.Logout();
+
+            connection.Disconnect();
+
+            Assert.That(loggedOut, Is.True);
+        }
+
+        [Test]
+        public void Can_GetQuote_Form_Server()
+        {
+            // Don't forget to run packages\ClientAPI\Server.exe
+            var connection = new ClientAPI.ClientConnection();
+            connection.Connect("127.0.0.1", 34567);
+
+            connection.Login("trademaker01", "t01m975kt");
+
+            var quote = new ClientLibrary.Quote();
+            var success = connection.GetQuote("PETR4", out quote);
+            
+            connection.Logout();
+
+            connection.Disconnect();
+
+            Assert.That(success, Is.True);
+            Assert.That(quote, Is.Not.Null);
+            Assert.That(quote.Object, Is.EqualTo("PETR4"));
+        }
+
+        [Test]
+        public void Can_StartQuoteStreaming_Form_Server()
+        {
+            // Don't forget to run packages\ClientAPI\Server.exe
+            var connection = new ClientAPI.ClientConnection();
+            connection.Connect("127.0.0.1", 34567);
+
+            connection.Login("trademaker01", "t01m975kt");
+
+            var success = connection.StartQuoteStreaming("PETR4");
+
+            connection.Logout();
+
+            connection.Disconnect();
+
+            Assert.That(success, Is.True);
+        }
+
+        [Test]
+        public void Can_StopQuoteStreaming_Form_Server()
+        {
+            // Don't forget to run packages\ClientAPI\Server.exe
+            var connection = new ClientAPI.ClientConnection();
+            connection.Connect("127.0.0.1", 34567);
+
+            connection.Login("trademaker01", "t01m975kt");
+
+            connection.StartQuoteStreaming("PETR4");
+            var success = connection.StopQuoteStreaming("PETR4");
+
+            connection.Logout();
+
+            connection.Disconnect();
+
+            Assert.That(success, Is.True);
+        }
+    }
+}
---
+++ TestDotNet.Quotes.UI/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClientAPI;
+using ClientLibrary;
+
+namespace TestDotNet.Quotes.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string UserName = "trademaker01";
+        private const string Password = "t01m975kt";
+        private readonly ClientConnection _connection;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            _connection = new ClientConnection();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            DisconnectAndLogout();
+            ConnectAndLogin();
+            LoadQuote();
+            StartStreaming();
+        }
+
+        private void quotes_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            var quote = (Quote)e.Row.Item;
+            switch (quote.Direction)
+            {
+                case "+":
+                    ChangeRowColor(e,
+                        Color.FromRgb(0, 255, 0),
+                        Color.FromRgb(255, 255, 255));
+                    break;
+                case "-":
+                    ChangeRowColor(e,
+                        Color.FromRgb(255, 0, 0),
+                        Color.FromRgb(255, 255, 255));
+                    break;
+                case "=":
+                    ChangeRowColor(e,
+                        Color.FromRgb(255, 255, 255),
+                        Color.FromRgb(0, 0, 0));
+                    break;
+            }
+
+        }
+
+        void connection_OnQuoteStreaming(Quote quote)
+        {
+            quotes.ItemsSource = new[] { quote };
+        }
+        
+        private void StartStreaming()
+        {
+            _connection.OnQuoteStreaming += connection_OnQuoteStreaming;
+            _connection.StartQuoteStreaming(objectName.Text);
+        }
+
+
+
+        private void LoadQuote()
+        {
+            var quote = new Quote();
+            _connection.GetQuote(objectName.Text, out quote);
+            if (null != quote)
+                quotes.ItemsSource = new[] { quote };
+            else
+            {
+                MessageBox.Show(_connection.ErrorMessage,"Error",MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void DisconnectAndLogout()
+        {
+            _connection.Logout();
+            _connection.Disconnect();
+        }
+
+        private void ConnectAndLogin()
+        {
+            _connection.Connect(host.Text, ushort.Parse(port.Text));
+            _connection.Login(UserName, Password);
+        }
+
+        private static void ChangeRowColor(DataGridRowEventArgs e,
+            Color backgroundColor,
+            Color foregroundColor)
+        {
+            e.Row.Background = new SolidColorBrush(backgroundColor);
+            e.Row.Foreground = new SolidColorBrush(foregroundColor);
+        }
+    }
+}
